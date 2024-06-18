35e0ba3b17bf4fda42207f379fd5d0b71d2ba44b
Atif Alam Ansari <101616231+Atif0703@users.noreply.github.com>
2024-06-19 02:37:38 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+		'x-rapidapi-key': 'e1aead02d9msh47bab47930d1f8ep1fb2e7jsn85dcaf1cd06c',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "";"0fe0fcd2eb6a123d69bca4755646957d"
